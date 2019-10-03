f5ca0c26998bd8a909c2c0d9b21e6d1312f05fa1
Shaista Khalid <Shaizu10@gmail.com>
2019-10-02 22:44:00 -0400

    first commit

---
+++ Lab3/routes/Dad.js
@@ -0,0 +1,15 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    if (req.query)
+        var name = "Dad"
+
+
+    res.render('Dad', { name: name });
+});
+
+module.exports = router;
